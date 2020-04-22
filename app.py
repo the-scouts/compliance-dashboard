287a75e05eb18efa65f9bb4c507c2e9fe06c3ed4
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-22 20:24:44 +0100

    Move app stuff to app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import dash
-import dash_auth
+# import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+import index
 
-app = dash.Dash(
+app: dash.Dash = dash.Dash(
     __name__,
     external_stylesheets=[dbc.themes.UNITED]
 )
-server = app.server
 # app.config.suppress_callback_exceptions=True
+# server = app.server
 
 # # Keep this out of source code repository - save in a file or a database
 # VALID_USERNAME_PASSWORD_PAIRS = {
@@ -16,3 +19,34 @@ server = app.server
 # }
 # auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
 
+app.index_string = """ 
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>Compliance Dashboard Generator - DEVELOPMENT (Adam Turner)</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+"""
+
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
+
+    # content will be rendered in this element
+    html.Div(id='page-content')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    index.run_app(app)
--- index.py
+++ index.py
@@ -1,61 +1,26 @@
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
-
-# see https://community.plotly.com/t/12463/2
-from app import app
+from dash import Dash
 
 from pages.dashboard_generator import DashbordGenerator
 from pages.homepage import Homepage
 
-# see https://dash.plot.ly/external-resources to alter the header, footer and favicon
-app.index_string = """ 
-<!DOCTYPE html>
-<html>
-    <head>
-        {%metas%}
-        <title>Compliance Dashboard Generator - DEVELOPMENT (Adam Turner)</title>
-        {%favicon%}
-        {%css%}
-    </head>
-    <body>
-        {%app_entry%}
-        <footer>
-            {%config%}
-            {%scripts%}
-            {%renderer%}
-        </footer>
-    </body>
-</html>
-"""
 
-app.layout = html.Div([
-    # represents the URL bar, doesn't render anything
-    dcc.Location(id='url', refresh=False),
-
-    # content will be rendered in this element
-    html.Div(id='page-content')
-])
-
-dg = DashbordGenerator()
-dg.set_info("County Team", "October 2019", "Central Yorkshire")
-dg.set_people(305, 389)
+def run_app(app: Dash):
+    dg = DashbordGenerator(app)
+    dg.set_info("County Team", "October 2019", "Central Yorkshire")
+    dg.set_people(305, 389)
 
 
-@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/report':
-        return dg.get_dashboard()
-    else:
-        return Homepage()
-
-
-# @app.callback(Output('output',  'children'), [Input('pop_dropdown', 'value')])
-# def update_graph(city):
-#     return build_graph(city)
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    @app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+    def display_page(pathname):
+        if pathname == '/report':
+            return dg.get_dashboard()
+        else:
+            return Homepage(app)
 
     dg.set_people(1, 2)
+
+
+
+
+
