3f17cd75097816d7b59a955db7c11adb50d2dea4
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-23 01:35:13 +0100

    Update navigation

--- page_components/navbar.py
+++ page_components/navbar.py
@@ -1,4 +1,3 @@
-import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 
@@ -7,34 +6,15 @@ from dash import Dash
 
 
 def Navbar(app: Dash):
-    return dbc.NavbarSimple(
-        [
-            dbc.NavItem(dbc.NavLink("New dashboard", href="/new")),
-            dbc.NavItem(dbc.NavLink("View dashboard", href="/report")),
-        ],
-        brand="Compliance: The Basics",
-        brand_href="/home",
-        sticky="top",
-    )
-
-
-def Navbar2(app: Dash):
-    return dbc.Nav([
+    return html.Nav([
         html.Div([
             dcc.Link([
-                html.Div([
-                    html.Img(src=app.get_asset_url("logo-purple.png"), className="nav-brand-img"),
-                    html.Span("Compliance: The Basics", className="nav-brand-text")
-                ], className="nav-brand-div"),
-            ], href="/home", className="nav-brand-link"),
-
-
-            html.Div([
-                html.Ul([
-                    dcc.Link("New dashboard", href="/new"),
-                    dcc.Link("View dashboard", href="/report")
-                ], className="links-list")
-            ], className="links")
-
-        ], className="row gs-header"),
-    ])
+                html.Img(src=app.get_asset_url("logo-black.png")),
+                html.Span("Compliance: The Basics")
+            ], href="/home", className="nav-brand"),
+            html.Ul([
+                dcc.Link("New dashboard", href="/new"),
+                dcc.Link("View dashboard", href="/report")
+            ], className="nav-links")
+        ], className="nav-container")
+    ], className="navigation bg-nav")
