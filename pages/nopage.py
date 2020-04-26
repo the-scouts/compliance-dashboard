cee9b46438006459f3b9e5622144d52f03876784
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-26 18:41:29 +0100

    Support 404 page

--- pages/nopage.py
+++ pages/nopage.py
@@ -1,4 +1,5 @@
 import dash_html_components as html
+import dash_core_components as dcc
 from page_components.navbar import Navbar
 
 # Type hints:
@@ -9,5 +10,14 @@ def noPage(app: Dash):
     return html.Div([
         # CC Header
         Navbar(app),
-        html.P(["404 Page not found"])
+        page_not_found()
     ], className="no-page")
+
+
+def page_not_found():
+    return html.Div([
+        html.H3("The page you requested does't exist."),
+        dcc.Link([
+            html.H4("Click here to go to the start page")
+        ], href="/home")
+    ], className="err-container")
