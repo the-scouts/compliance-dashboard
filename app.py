3e674ec1e9917729dc552a8ede06493738804ed2
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-05-08 18:27:13 +0100

    Make server accessible to gunicorn

--- app.py
+++ app.py
@@ -10,87 +10,77 @@ import index
 from config import user, password
 
 
-normal_layout = html.Div([
+app: dash.Dash = dash.Dash(__name__)
+server = app.server
+
+VALID_USERNAME_PASSWORD_PAIRS = {
+    user: password,
+}
+auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
+
+app.index_string = """ 
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>Compliance Dashboard Generator - DEVELOPMENT (Adam Turner)</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+"""
+
+app.validation_layout = html.Div([
+    html.Div(id='page-content'),
+    # html.Div(id="page-store", style={"display": "none"}),
+    html.Ul(id="nav-list"),
+    html.Button(id='button'),
+    html.Button(id="report-download"),
+    dcc.Location(id='url', refresh=False),
+    dcc.Store(id="report-query", storage_type="local"),
+    # dcc.Store(id="page-store", storage_type="local"),
+    dcc.Store(id="contents-compliance-report"),
+    dcc.Store(id="contents-training-report"),
+    dcc.Upload(id='upload-compliance-report'),
+    dcc.Upload(id='upload-training-report'),
+    dcc.Input(id="input-title"),
+    dcc.Input(id="input-location"),
+    dcc.Input(id="input-disclosures"),
+    # dcc.Interval(id='interval-component'),
+    # visdcc.Run_js(id='javascript'),
+])
+
+app.layout = html.Div([
     # represents the URL bar, doesn't render anything
     dcc.Location(id='url', refresh=False),
 
+    # html.Div(id="page-store", style={"display": "none"}),
+    # dcc.Interval(id='interval-component', interval=2000),
     dcc.Store(id="report-query", storage_type="local"),
+    # dcc.Store(id="page-store", storage_type="local", data="0"),
+    dcc.Store(id="contents-compliance-report"),
+    dcc.Store(id="contents-training-report"),
+    # visdcc.Run_js(id='javascript'),
 
     # content will be rendered in this element
     html.Div(id='page-content')
 ])
 
+# @server.route('/download/<path:path>')
+# def serve_report(path):
+#     """Serve a file from the upload directory."""
+#     return flask.send_from_directory(DOWNLOAD_DIR, path, as_attachment=True)
+
+idx = index.DGIndex(app)
+idx.run_app(app)
 
 if __name__ == '__main__':
-    app: dash.Dash = dash.Dash(__name__)
-    server = app.server
-
-    VALID_USERNAME_PASSWORD_PAIRS = {
-        user: password,
-    }
-    auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
-
-    app.index_string = """ 
-    <!DOCTYPE html>
-    <html>
-        <head>
-            {%metas%}
-            <title>Compliance Dashboard Generator - DEVELOPMENT (Adam Turner)</title>
-            {%favicon%}
-            {%css%}
-        </head>
-        <body>
-            {%app_entry%}
-            <footer>
-                {%config%}
-                {%scripts%}
-                {%renderer%}
-            </footer>
-        </body>
-    </html>
-    """
-
-    app.validation_layout = html.Div([
-        html.Div(id='page-content'),
-        # html.Div(id="page-store", style={"display": "none"}),
-        html.Ul(id="nav-list"),
-        html.Button(id='button'),
-        html.Button(id="report-download"),
-        dcc.Location(id='url', refresh=False),
-        dcc.Store(id="report-query", storage_type="local"),
-        # dcc.Store(id="page-store", storage_type="local"),
-        dcc.Store(id="contents-compliance-report"),
-        dcc.Store(id="contents-training-report"),
-        dcc.Upload(id='upload-compliance-report'),
-        dcc.Upload(id='upload-training-report'),
-        dcc.Input(id="input-title"),
-        dcc.Input(id="input-location"),
-        dcc.Input(id="input-disclosures"),
-        # dcc.Interval(id='interval-component'),
-        # visdcc.Run_js(id='javascript'),
-    ])
-
-    app.layout = html.Div([
-        # represents the URL bar, doesn't render anything
-        dcc.Location(id='url', refresh=False),
-
-        # html.Div(id="page-store", style={"display": "none"}),
-        # dcc.Interval(id='interval-component', interval=2000),
-        dcc.Store(id="report-query", storage_type="local"),
-        # dcc.Store(id="page-store", storage_type="local", data="0"),
-        dcc.Store(id="contents-compliance-report"),
-        dcc.Store(id="contents-training-report"),
-        # visdcc.Run_js(id='javascript'),
-
-        # content will be rendered in this element
-        html.Div(id='page-content')
-    ])
-
-    # @server.route('/download/<path:path>')
-    # def serve_report(path):
-    #     """Serve a file from the upload directory."""
-    #     return flask.send_from_directory(DOWNLOAD_DIR, path, as_attachment=True)
-
-    idx = index.DGIndex(app)
-    idx.run_app(app)
     app.run_server(debug=True)
