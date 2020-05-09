2212a19bcaf32c8a226f801cc678098d598c4e86
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-05-09 04:15:02 +0100

    Update styles

--- src/auth.py
+++ src/auth.py
@@ -22,8 +22,8 @@ def authenticate_user(credentials):
 
     # replace with your code
     username = credentials[0]
-    password = credentials[1]
-    authed = (username in users) and (password == users[username])
+    pw = credentials[1]
+    authed = (username in users) and (pw == users[username])
 
     return authed
 
@@ -36,17 +36,14 @@ def validate_login_session(f):
     """
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if session.get('authed'):
+        if session.get("authed"):
             return f(*args, **kwargs)
         return html.Div(
-            dbc.Row([
-                dbc.Col([
-                    dbc.Card([
-                        html.H2('401 - Unauthorized', className='card-title'),
-                        html.A(dcc.Link('Login', href='/login'))
-                    ], body=True)
-                ], width=5),
-            ], justify='center')
+            html.Div([
+                html.H2("401 - Unauthorised", className="card-title"),
+                html.H4(dcc.Link("Login", href="/login"))
+            ], className="page-container vertical-center static-container"),
+            className="page"
         )
 
     return wrapper
@@ -54,32 +51,27 @@ def validate_login_session(f):
 
 # login layout content
 def login_layout():
-    return html.Div([
-        dbc.Container([
-            dbc.Row(
-                dbc.Col(
-                    dbc.Card([
-                        html.H4('Login', className='card-title'),
-                        dbc.Input(id='login-username', placeholder='User'),
-                        dbc.Input(id='login-password', placeholder='Assigned password', type='password'),
-                        dbc.Button('Submit', id='login-button', color='success', block=True),
-                        html.Br(),
-                        html.Div(id='login-alert')
-                        ], body=True),
-                    width=6),
-                justify='center')
-            ])
-        ])
+    return html.Div(
+        html.Div([
+            html.H4("Login", className="card-title"),
+            dbc.Input(id="login-username", placeholder="Username", type="text"),
+            dbc.Input(id="login-password", placeholder="Password", type="password"),
+            dbc.Button("Submit", id="login-button", color="success", block=True),
+            html.Br(),
+            html.Div(id="login-alert")
+        ], className="page-container vertical-center static-container"),
+        className="page"
+    )
 
 
 def setup_auth_callbacks(app: dash.Dash):
     # authenticate
     @app.callback(
-        [Output('login-url', 'pathname'),
-         Output('login-alert', 'children')],
-        [Input('login-button', 'n_clicks')],
-        [State('login-username', 'value'),
-         State('login-password', 'value')],
+        [Output("login-url", "pathname"),
+         Output("login-alert", "children")],
+        [Input("login-button", "n_clicks")],
+        [State("login-username", "value"),
+         State("login-password", "value")],
         prevent_initial_call=True)
     def login_auth(_, username, pw):
         """
@@ -89,16 +81,7 @@ def setup_auth_callbacks(app: dash.Dash):
         """
         credentials = (username, pw)
         if authenticate_user(credentials):
-            session['authed'] = True
-            return '/home', ''
-        session['authed'] = False
-        return dash.no_update, dbc.Alert('Incorrect credentials.', color='danger', dismissable=True)
-
-    # # logout
-    # @app.callback(Output('url', 'pathname'),
-    #               [Input('logout-button', 'n_clicks'), ],
-    #               prevent_initial_call=True)
-    # def logout_(_):
-    #     """clear the session and send user to login"""
-    #     session['authed'] = False
-    #     return '/login'
+            session["authed"] = True
+            return "/home", ""
+        session["authed"] = False
+        return dash.no_update, dbc.Alert("Incorrect credentials.", color="danger", dismissable=True)
