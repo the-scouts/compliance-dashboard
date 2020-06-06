83e3f10afa0c21e53903808d0b6e6756bef48199
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-06 05:57:23 +0100

    Add assets dir

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ from functools import reduce
 import json
 
 PROJECT_ROOT = Path(__file__).parent.parent.absolute()
+ASSETS_DIR = PROJECT_ROOT / "assets"
 DATA_ROOT = PROJECT_ROOT / "data"
 DOWNLOAD_DIR = DATA_ROOT / "download"
 UPLOAD_DIR = DATA_ROOT / "upload"
