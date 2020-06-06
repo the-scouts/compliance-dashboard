33e65dcb13c27cf56a55ff34424032e60f7e937d
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-06 05:57:55 +0100

    Add cache like accessors

--- src/config.py
+++ src/config.py
@@ -13,3 +13,46 @@ env = os.environ
 secret_key = env.get("secret_key")
 user = env.get("user")
 password = env.get("pass")
+
+
+def get_by_path(root, items):
+    """Access a nested object in root by item sequence."""
+    return reduce(lambda d, k: d.get(k), items, root)
+
+
+def set_by_path(root: dict, items, value):
+    """Set a value in a nested object in root by item sequence."""
+    for key in items[:-1]:
+        root.setdefault(key, {})
+        root = root[key]
+    root[items[-1]] = value
+
+
+def get_from_cache(*path):
+    """Access a value from the global cache"""
+    try:
+        return get_by_path(global_path_cache_dict, path)
+    except AttributeError:
+        return None
+
+
+def set_to_cache(*path, value=None):
+    """Access a value from the global cache"""
+    return set_by_path(global_path_cache_dict, path, value)
+
+
+def save_cache():
+    DATA_ROOT.joinpath("cache", "cache.json").write_text(json.dumps(global_path_cache_dict), encoding="UTF8")
+
+
+def load_cache() -> dict:
+    try:
+        return json.loads(DATA_ROOT.joinpath("cache", "cache.json").read_text(encoding="UTF8"))
+    except (FileNotFoundError, json.JSONDecodeError):
+        return dict(
+            b64_cache=dict(),
+            session_cache=dict(),
+        )
+
+
+global_path_cache_dict = load_cache()
